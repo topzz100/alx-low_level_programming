a34ea44b177d38151bf2ab16e6a6b1ff930bef23
topzz100 <adenekantope@gmail.com>
2022-07-06 14:48:43 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,14 @@
 
 int main()
 {
-	puts('_putchar');
+	putchar('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
     	putchar ('\n');
 
 	      return 0;
