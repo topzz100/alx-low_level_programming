e365d4bc3ce8ddf04949816855e6fcadfe43faf1
topzz100 <adenekantope@gmail.com>
2022-07-06 18:07:40 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
-	print_alphabet();
-	return(0);
-}
-void print_alphabet(){
+void print_alphabet(void){
 
 	char c;
 	for (c = 'a'; c <= 'z'; ++c)
 		putchar(c);
 	putchar('\n');
-	return;
+	return(0);
 
 }
+int main(void)
+{
+	print_alphabet();
+	return(0);
+}
+
+
