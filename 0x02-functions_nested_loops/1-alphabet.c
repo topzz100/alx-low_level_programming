5c4c19d3894a9f7f7fa210fe4cc6bc6683ecb92a
topzz100 <adenekantope@gmail.com>
2022-07-06 17:57:57 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,12 @@ int main(void) {
 	print_alphabet();
 	return(0);
 }
-void print_alphabet(void){
+void print_alphabet(){
 
 	char c;
 	for (c = 'a'; c <= 'z'; ++c)
 		putchar(c);
 	putchar('\n');
+	return;
+
 }
